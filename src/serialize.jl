493fd37cbc8e0930adb93aaa940f90bcb2728ec6
Felipe Noronha <felipenoris@gmail.com>
2019-08-01 16:26:57 -0300

    docstrings

--- src/serialize.jl
+++ src/serialize.jl
@@ -1,4 +1,30 @@
 
+"""
+    serialize(val) :: BSON
+
+Encodes a Julia value `val` into BSON format.
+
+    serialize(io::IO, val)
+
+Writes a Julia value `val` into `io` using BSON format.
+"""
+function serialize end
+
+"""
+    deserialize(doc::Union{BSON, Dict}, m::Module=Main)
+
+Decodes a BSON that was previously encoded using `BSONSerializer.serialize` method
+into a Julia value.
+
+This method also accepts a `Dict` as input, given that
+you may want to decode just a portion of a BSON document.
+
+Type information is encoded as a String in the BSON document,
+so the `deserialize` method uses `m.eval` to convert that to a Julia type,
+where `m` is the 2nd argument of the `deserialize` method.
+"""
+function deserialize end
+
 function serialize(val::T) :: BSON where {T}
     return serialize(Serializable(val))
 end
@@ -14,6 +40,12 @@ function deserialize(bson::Union{BSON, Dict}, m::Module=Main)
     return deserialize(bson, Serializable{datatype}, m)
 end
 
+"""
+    roundtrip(val:::T) :: T
+
+Applies `serialize` and `deserialize` to `val`.
+Useful for testing.
+"""
 function roundtrip(val::T) where {T}
     return deserialize(serialize(Serializable(val)), Serializable{T})
 end
